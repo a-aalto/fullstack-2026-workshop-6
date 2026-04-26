9946f50194de4383c4b97784d6b5ea49687c84c8
Antti Aalto <antti.johannes.aalto@student.laurea.fi>
2026-04-26 15:09:37 +0300

    Users can now create a new post. After successfull post, the user is redirected to PostPage

--- frontend/src/pages/NewPostPage.jsx
+++ frontend/src/pages/NewPostPage.jsx
@@ -2,34 +2,55 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import PostForm from '../components/PostForm.jsx'
 
-// TODO (student): Implement create flow (POST /api/posts).
-// Suggested steps:
-// 1) Read form values in handleSubmit.
-// 2) POST JSON body to /api/posts.
-// 3) On success, navigate to /posts/:id.
-// 4) Show an error message on failure.
 function NewPostPage() {
-  const navigate = useNavigate()
-  const [submitting, setSubmitting] = useState(false)
-  const [error, setError] = useState(null)
+	const navigate = useNavigate()
+	const [submitting, setSubmitting] = useState(false)
+	const [error, setError] = useState(null)
 
-  async function handleSubmit(e) {
-    e.preventDefault()
-    setSubmitting(true)
-    setError(null)
+	async function handleSubmit(e) {
+		e.preventDefault()
+		setSubmitting(true)
+		setError(null)
 
-    // TODO (student): Implement this submit logic.
-    setError('TODO: implement POST /api/posts in NewPostPage')
-    setSubmitting(false)
-  }
+		try {
+			const form = e.target
 
-  return (
-    <div>
-      <h1 className="page-title">New post</h1>
-      {error && <p className="status-msg error">{error}</p>}
-      <PostForm onSubmit={handleSubmit} submitting={submitting} />
-    </div>
-  )
+			const newPost = {
+				title: form.title.value,
+				author: form.author.value,
+				content: form.content.value,
+			}
+
+			const response = await fetch('/api/posts', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(newPost),
+			})
+
+			if (!response.ok) {
+				throw new Error('Failed to create a post')
+			}
+			const data = await response.json()
+
+			const postID = data._id
+
+			navigate(`/posts/${postID}`)
+		} catch (err) {
+			setError(err.message)
+
+			setSubmitting(false)
+		}
+	}
+
+	return (
+		<div>
+			<h1 className='page-title'>New post</h1>
+			{error && <p className='status-msg error'>{error}</p>}
+			<PostForm onSubmit={handleSubmit} submitting={submitting} />
+		</div>
+	)
 }
 
 export default NewPostPage
